a1626ff8fbe7016ee1af4eb292aae6e26778bb4e
Rui <rui.silva1004@gmail.com>
2023-10-10 20:48:40 +0100

    Updated requirements.txt

--- core/urls.py
+++ core/urls.py
@@ -18,18 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from . import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api/', include('api.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
-    #path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    #path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    #path('api/signup/', signup_user, name='signup_user'),
     path('', include('website.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
