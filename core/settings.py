56ec75f46fe03c3df03909172e078649216aa3d6
Rui <rui.silva1004@gmail.com>
2023-10-13 14:00:32 +0100

    Setting changes

--- core/settings.py
+++ core/settings.py
@@ -142,9 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # durante development
-#STATIC_ROOT = os.path.join(BASE_DIR, 'pasta') --> quando estiver em production, NÃO ESQUECER DE ALTERAR urls.py, o document_root
+STATIC_URL = '/static/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # durante development
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
